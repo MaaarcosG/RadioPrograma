33a5b95410335f3177094835997e2d43bc6a62ba
MaaarcosG <33351664+MaaarcosG@users.noreply.github.com>
2018-01-16 10:03:03 -0600

    Intefaz de la Radio

---
+++ src/Interfaz_Radio.java
@@ -0,0 +1,11 @@
+
+public interface Interfaz_Radio {
+	public String frecAdelante();
+	public String frecAtras();
+	public boolean apagar();
+	public boolean encender();
+	public String cambioFrecuencia();
+	public void guardarFrecAm(int frec, int pos);
+	public void guardarFrecFm(float frec, int pos);
+	public String mostrasEstacion();
+}
