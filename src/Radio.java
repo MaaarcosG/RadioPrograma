8f58d727c58b1aeffc1e31bfb181f104ffe51812
MaaarcosG <33351664+MaaarcosG@users.noreply.github.com>
2018-01-16 12:33:25 -0600

    Clase Radio

--- src/Radio.java
+++ src/Radio.java
@@ -3,70 +3,57 @@
  */
 
 /**
- * Esta clase contiene los set y gets de los parametros que se encuentran 
- * dentro de la clase intefaz, tambien los inicializa.
- * 
- * @author Marcos Gutierrez 			17909
- * @since 16 enero 2018
- * @version beta 3.2
+ * @author Marcos Gutierrez 		Carne: 17909
+ * @since 16 de enero 2018
+ * @version beta 2.0
+ *
  */
-public class Radio implements Interfaz_Radio {
-
-	public Radio() {
-		// TODO Auto-generated constructor stub
-	}
-	/**
-	 * @return 
-	 */
+public class Radio implements Interfaz_Radio{
 	@Override
 	public String frecAdelante() {
+		// TODO Auto-generated method stub
 		return null;
 	}
-	/**
-	 * @return
-	 */
+
 	@Override
 	public String frecAtras() {
+		// TODO Auto-generated method stub
 		return null;
 	}
-	/**
-	 * @return
-	 */
+
 	@Override
 	public boolean apagar() {
+		// TODO Auto-generated method stub
 		return false;
 	}
-	/**
-	 * @return false
-	 */
+
 	@Override
 	public boolean encender() {
+		// TODO Auto-generated method stub
 		return false;
 	}
-	/**
-	 * @return 
-	 */
+
 	@Override
 	public String cambioFrecuencia() {
+		// TODO Auto-generated method stub
 		return null;
 	}
-	
+
 	@Override
 	public void guardarFrecAm(int frec, int pos) {
-		
+		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
 	public void guardarFrecFm(float frec, int pos) {
-		
+		// TODO Auto-generated method stub
 		
 	}
-	/**
-	 * @return 
-	 */
+
 	@Override
 	public String mostrasEstacion() {
+		// TODO Auto-generated method stub
 		return null;
 	}
 
